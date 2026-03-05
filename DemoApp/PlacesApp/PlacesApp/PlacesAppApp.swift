8ddb40a2c0a8a0400cc7567a652f5b7e6866dd03
Sander Korebrits <s.korebrits@designlapp.com>
2026-03-05 11:11:24 +0100

    Add PlacesApp project

---
+++ DemoApp/PlacesApp/PlacesApp/PlacesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesAppApp.swift
+//  PlacesApp
+//
+//  Created by Sander Korebrits on 05/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct PlacesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DemoApp/PlacesApp/PlacesAppTests/PlacesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesAppTests.swift
+//  PlacesAppTests
+//
+//  Created by Sander Korebrits on 05/03/2026.
+//
+
+import Testing
+@testable import PlacesApp
+
+struct PlacesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
