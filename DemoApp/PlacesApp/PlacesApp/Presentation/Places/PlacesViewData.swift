2fb223a42317078ddb14f668eb8e13158807a068
Sander Korebrits <s.korebrits@designlapp.com>
2026-03-08 23:30:55 +0100

    remove unused protocol

--- DemoApp/PlacesApp/PlacesApp/Presentation/Places/PlacesViewData.swift
+++ DemoApp/PlacesApp/PlacesApp/Presentation/Places/PlacesViewData.swift
@@ -5,11 +5,7 @@
 //  Created by Sander Korebrits on 08/03/2026.
 //
 
-protocol ViewDataWithTitle {
-    var title: String { get }
-}
-
-struct PlacesViewData: ViewDataWithTitle {
+struct PlacesViewData {
     let title: String
     let cellViewData: [PlacesCellViewData]
 }
